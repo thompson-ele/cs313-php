d6747fc1c97cc400144128b27a8a0dc39bb4f28c
thompson-ele <ele.thompson12@gmail.com>
2017-01-19 22:45:07 -0800

    Debugging validation

--- web/teamwork/week3.php
+++ web/teamwork/week3.php
@@ -1,119 +1,216 @@
-<?php 
+<?php
+    // Config PHP To Display Errors
 	error_reporting(E_ALL);
 	ini_set("display_errors", 1);
+
+	// https://xkcd.com/327/
+    function sanitize($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+
 	$showForm = true;
+
+    // Associative array containing all majors.
 	$majors = array(
-				array(
-					"name" => "Computer Science", 
-					"val"  => "CS"
-					),
-				array(
-					"name" => "Web Design and Development", 
-					"val"  => "WDD"
-					),
-				array(
-					"name" => "Computer information Technology", 
-					"val"  => "CIT"
-					),
-				array(
-					"name" => "Computer Engineering", 
-					"val"  => "CE"
-					)
+      "CS"  => "Computer Science",
+      "WDD" => "Web Design and Development",
+      "CIT" => "Computer Information Technology",
+      "CE"  => "Computer Engineering"
 	);
+
+    // Associative array containing all continents.
 	$continents = array(
-    	array("name" => "North America", "val" => "na"),
-    	array("name" => "South America", "val" => "sa"),
-    	array("name" => "Europe",        "val" => "eu"),
-    	array("name" => "Asia",          "val" => "as"),
-    	array("name" => "Australia",     "val" => "au"),
-    	array("name" => "Africa",        "val" => "af"),
-    	array("name" => "Antarctica",    "val" => "an")
-    );   
-      
+    	"na" => "North America",
+    	"sa" => "South America",
+    	"eu" => "Europe",
+    	"as" => "Asia",
+    	"au" => "Australia",
+    	"af" => "Africa",
+    	"an" => "Antarctica"
+    );
+
 	$name;
 	$email;
+	$major;
 	$displayMajor;
 	$comments;
 	$displayContinents = array();
-	if (isset($_POST["Name"])) {
+
+    $nameErr = "";
+    $emailErr = "";
+    $continentErr = "";
+    $majorErr = "";
+
+    // If the form was posted with a Name variable...
+    // another option... if ($_SERVER["REQUEST_METHOD"] == "POST")
+	// then, sanitize and validate using emtpty()
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$showForm = false;
-		$name  = $_POST["Name"];
-		$email = $_POST["Email"];
-		foreach ($majors as $major) {
-			if ($_POST["Major"] === $major["val"]) {
-				$displayMajor = $major["name"];
-			}
-		}
-		$comments = $_POST["Comments"];
-      
-        foreach ($continents as $continent) {
-          	foreach ($_POST["Continents"] as $postCont) {
-                if ($postCont === $continent["val"]) {
-                    array_push($displayContinents, $continent["name"]);
-                }
-            }
+    
+      	$name = sanitize($_POST["Name"]);
+        $email = sanitize($_POST["Email"]);
+      	$major = sanitize($_POST["Major"]);
+        $continent = $_POST["Continents"];
+                                
+        if (empty($name)) { 
+          	$nameErr = "Name is Required"; 
+            $showForm = true;
+        } 
+        //else { $name = sanitize($_POST["Name"]); }
+            
+        if (empty($_POST["Email"])) { $emailErr = "Email is Required"; }
+        else if (!filter_var($_POST["Email"], FILTER_VALIDATE_EMAIL) === false) {
+          $emailErr = "Invalid Email Format"; 
+          $showForm = true;
+        //} else { 
+        // check if e-mail address is well-formed
+          //if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            //$emailErr = "Invalid email format"; 
+          //}
         }
+      
+        if (empty($_POST["Major"])) { 
+          $majorErr = "Major is Required";
+          $showForm = true; } 
+        //else {  }
+      
+        if (empty($_POST["Continent"])) { 
+          $continentErr = "Continent is Required"; 
+          $showForm = true;} 
+        //else {  }
+      
+        //$showForm = false;
+		//$name  = sanitize($_POST["Name"]);
+		//$email = sanitize($_POST["Email"]);
+      
+
+        // Iterate through majors and store display name
+        $displayMajor = $majors[$major];
+		$comments = sanitize($_POST["Comments"]);
+
+        // Iterate through continents and build array of display names
+        foreach ($_POST["Continents"] as $postCont) {
+          array_push($displayContinents, sanitize($continents[$postCont]));
+        }
+        // Concatenate contintents together using a comma
       	$displayContString = implode(", ", $displayContinents);
 	}
- ?>
+?>
 
 <!DOCTYPE html>
 <html lang="en-us">
 	<head>
-		<title>Form!</title>
+		<title>Simple Survey</title>
+  
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<?php if ($showForm): ?>
-			<form action="week3.php" method="POST">
-				<label for="Name">Name: </label>
-				<input type="text" name="Name" id="Name" placeholder="Name">
-				<br>
-				<br>
-				<label for="Email">Email: </label>
-				<input type="text" name="Email" id="Email" placeholder="Email">
-				<br>
-				<br>
-				<label for="Major">Major: </label>
-				<br>
-				<?php foreach ($majors as $major): ?>
-					<input type="radio" name="Major" id="<?php echo $major["val"]; ?>" value="<?php echo $major["val"]; ?>">
-					<label for="<?php echo $major["val"]; ?>"><?php echo $major["name"] ?></label>	
-					<br>
-				<?php endforeach ?>
-				<br>
-				<label for="Continents">Continents: </label>
-				<br>
-   				<?php foreach ($continents as $continent): ?>
-					<input type="checkbox" name="Continents[]" id="<?php echo $continent["val"]; ?>" value="<?php echo $continent["val"]; ?>">
-					<label for="<?php echo $continent["val"]; ?>"><?php echo $continent["name"] ?></label>	
-					<br>
-				<?php endforeach ?>
-				
-				<label for="Comments">Comments</label>
-				<br>
-				<textarea name="Comments" placeholder="Comments"></textarea>
-				<br>
-				<br>
-				<input type="submit" name="Submit" value="Submit">
-			</form>
-		<?php else: ?>
-			<div class="result">
-				<p>
-					Name: <?php echo $name; ?>
-				</p>
-				<p>
-					Email: <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
-				</p>
-				<p>
-					Major: <?php echo $displayMajor; ?>
-				</p>
-				<p>
-					Continents: <?php echo $displayContString; ?>
-				</p>
-				<p>
-					Comments: <?php echo $comments; ?>
-				</p>
-			</div>
-		<?php endif; ?>
+  		<div class="container body-content">
+          <?php // DISPLAYING FORM ?>
+          <?php if ($showForm): ?>
+          <div class="row">
+              <div class="col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-6 col-lg-offset-0">
+                  <div class="panel panel-info" style="@borderColor border-width: 2px;">
+                      <div class="panel-heading">
+                          <h3 class="panel-title" style="font-weight: bolder;">
+                              Input Survey
+                          </h3>
+                      </div>
+                      <div class="panel-body">
+                          <div class="col-md-12">
+                            <form action="" method="POST">
+                            <?php // use isset() here to populate the fields with previous values ?>
+                            <div class="form-group">
+                                <label class="control-label" for="Name">Name *</label>
+                                <input class="form-control" type="text" name="Name" id="Name" placeholder="Name" value="<?php echo $name; ?>" required>
+                                <span class="error"><?php echo $nameErr; ?></span>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="control-label" for="Email">Email *</label>
+                                <input class="form-control" type="email" name="Email" id="Email" placeholder="Email" value="<?php echo $email; ?>" required>
+                                <span class="error"><?php echo $emailErr; ?></span>
+                            </div>
+
+                            <label class="control-label" for="Major">Major *</label>
+                            <br>
+                            <?php // Build list of majors - radio buttons ?>
+                            <?php foreach ($majors as $code => $name): ?>
+                                <div class="radio">
+                                    <input  type="radio"
+                                            name="Major"
+                                            id="<?php echo $code; ?>"
+                                            value="<?php echo $code; ?>"
+                                            required
+                                			<?php if($code == $major) { echo ' checked'; } ?>>
+                                    <label class="control-label" for="<?php echo $code; ?>"><?php echo $name ?></label>
+                                </div>
+                            <?php endforeach ?>
+                            <br>
+
+                            <label class="control-label" for="Continents">Continents </label>
+                            <br>
+                            <?php // Build list of continents - checkboxes ?>
+                            <?php foreach ($continents as $code => $name): ?>
+                                <div class="checkbox">
+                                    <input type="checkbox"
+                                           name="Continents[]"
+                                           id="<?php echo $code; ?>"
+                                           value="<?php echo $code; ?>"
+                                           <?php if($code == 'na') { echo ' required'; } ?>>
+                                    <label  for="<?php echo $code; ?>"><?php echo $name; ?></label>
+                                </div>
+                            <?php endforeach ?>
+
+                            <div class="form-group">
+                                <label class="control-label" for="Comments">Comments</label>
+                                <textarea class="form-control" name="Comments" value="<?php echo $comments; ?>" placeholder="Comments"></textarea>
+                            </div>
+                            <input class="btn btn-primary" type="submit" name="Submit" value="Submit">
+                          </div>
+                      </div>
+                      <div class="panel-footer">
+                          <div style="font-size: smaller;">
+                              &nbsp;
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          </form>
+          <?php // DISPLAYING RESULTS ?>
+          <?php else: ?>
+          <div class="row">
+              <div class="col-md-12 col-xs-offset-0 col-sm-offset-0 col-md-offset-6 col-lg-offset-0">
+                  <div class="panel panel-info" style="@borderColor border-width: 2px;">
+                      <div class="panel-heading">
+                          <h3 class="panel-title" style="font-weight: bolder;">
+                              Results
+                          </h3>
+                      </div>
+                      <div class="panel-body">
+                          <div class="col-md-12 result">
+                            <p> Name:       <?php echo $name;              ?> </p>
+                            <p> Email:    <a href="mailto:<?php echo $email; ?>">
+                                            <?php echo $email; ?> </a> </p>
+                            <p> Major:      <?php echo $displayMajor;      ?> </p>
+                            <p> Continents: <?php echo $displayContString; ?> </p>
+                            <p> Comments:   <?php echo $comments;          ?> </p>
+                          </div>
+                      </div>
+                      <div class="panel-footer">
+                          <div style="font-size: smaller;">
+                              &nbsp;
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <?php endif; ?>
+        </div>
 	</body>
 </html>
