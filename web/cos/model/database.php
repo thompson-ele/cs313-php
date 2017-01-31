a5de655a000e8ee0bfe7aa3ef1295d28ca2b82cd
Ele Thompson <ethompson@lbcc.edu>
2017-01-31 01:41:59 +0000

    Added database connection for COS project

--- web/cos/model/database.php
+++ web/cos/model/database.php
@@ -4,7 +4,7 @@
 $dbUrl = getenv('DATABASE_URL');
 
 if (empty($dbUrl)) {
-  //example localhost configuration URL with postgres username and a database called cs313db
+ // If accessing the database not on Heroku, use this config
  $dbUrl = "postgres://qebkqyxiqvsflj:c86156bcaa1172bc179daf4520f42a0549552cbe0de6ab44c9804f53e95af2a9@ec2-54-163-253-94.compute-1.amazonaws.com:5432/d2nre3mi3v2jrs";
 }
 
@@ -20,10 +20,12 @@ $dbName = ltrim($dbopts["path"],'/');
 
 // Create PDO object
 try {
- $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    // Sets PDO Error Mode, all errors are converted to Exceptions
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-catch (PDOException $ex) {
- print "<p>error: $ex->getMessage() </p>\n\n";
- die();
+catch (PDOException $e) {
+    echo $e->getMessage();
+    die();
 }
 ?>
